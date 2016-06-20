8577b25435ab3973683e91bac0d6039d7bb5e680
Pierre Marijon <pierre@marijon.fr>
2016-06-20 13:18:07 +0200

    Add ref version of each algorithms

--- generate_graph.R
+++ generate_graph.R
@@ -4,11 +4,11 @@ args <- commandArgs(trailingOnly = TRUE)
 
 data <- read.csv(args[1], sep=";")
 
-png(args[2], width=760, height=480, units="px")
+svg(args[2], width=14, height=10)
 par(mfrow=c(1,2))
 
 boxplot(data, ylab = "Time in millisecond", xlab = "Algorithme")
-ts.plot(data, col=c("red", "blue"), xlab = "replicate", ylab = "Time in millisecond")
-legend("topright", col = c("red", "blue"), legend = colnames(data), lwd=1, lty=c(1,1))
+ts.plot(data, col=c("orange", "red", "blue", "purple"), xlab = "replicate", ylab = "Time in millisecond")
+legend("topright", col = c("orange", "red", "blue", "purple"), legend = colnames(data), lwd=1, lty=c(1,1))
 
 tmp <- dev.off()
--- src/main.cpp
+++ src/main.cpp
@@ -17,9 +17,27 @@ std::string switch_transform(std::string seq)
 	    }
 	    return 'N';
 	});
-  
+
     return r;
 }
+
+std::string switch_transform_const_ref(const std::string& seq)
+{
+    std::string r(seq);
+    std::transform(seq.rbegin(), seq.rend(), r.begin(), [](char c) {
+	    switch(c) {
+	    case 'A': return 'T';
+	    case 'C': return 'G';
+	    case 'G': return 'C';
+	    case 'T': return 'A';
+	    default: return 'N';
+	    }
+	    return 'N';
+	});
+
+    return r;
+}
+
 
 class tab_revert
 {
@@ -67,20 +85,42 @@ public:
 		    ++first;
 		}
 
+	    return seq;
+	}
+
+    std::string run_ref(std::string& seq)
+	{
+	    auto first = seq.begin(), last = seq.end();
+
+	    while(true)
+		if(first == last || first == --last)
+		{
+		    if(seq.length() % 2)
+			*first = nuc2nuc[*first];
+		    return seq;
+		}
+		else
+		{
+		    *first = nuc2nuc[*first];
+		    *last = nuc2nuc[*last];
+		    std::iter_swap(first, last);
+		    ++first;
+		}
+
+	    return seq;
 	}
 };
 tab_revert *tab_revert::m_instance = 0;
 
 int main(int argc, char** argv)
 {
-    /* Init table */
-    
-    std::cout<<"switch_transform;tabular_revert"<<std::endl;
-
     /* Take argument */
     long long replicate = std::stoll(std::string(argv[1]));
     long long repeat = std::stoll(std::string(argv[2]));
     std::string seq(argv[3]);
+
+    /* Init table */
+    std::cout<<"switch_transform;switch_transform_const_ref;tabular_revert;tabular_revert_ref"<<std::endl;
 
     for(auto i = 0; i != replicate; i++)
     {
@@ -93,12 +133,32 @@ int main(int argc, char** argv)
 	auto elapsed = std::chrono::high_resolution_clock::now() - begin;
 
 	std::cout<<std::chrono::duration_cast<std::chrono::microseconds>(elapsed).count()<<";";
+
+	/* switch_transform_const_ref call */
+	begin = std::chrono::high_resolution_clock::now();
+	for(auto i = 0; i != repeat; i++)
+	{
+	    switch_transform_const_ref(seq);
+	}
+	elapsed = std::chrono::high_resolution_clock::now() - begin;
+
+	std::cout<<std::chrono::duration_cast<std::chrono::microseconds>(elapsed).count()<<";";
     
 	/* tab and revert */
 	begin = std::chrono::high_resolution_clock::now();
 	for(auto i = 0; i != repeat; i++)
 	{
 	    tab_revert::get_instance()->run(seq);
+	}
+	elapsed = std::chrono::high_resolution_clock::now() - begin;
+
+	std::cout<<std::chrono::duration_cast<std::chrono::microseconds>(elapsed).count()<<";";
+
+	/* tab and revert */
+	begin = std::chrono::high_resolution_clock::now();
+	for(auto i = 0; i != repeat; i++)
+	{
+	    tab_revert::get_instance()->run_ref(seq);
 	}
 	elapsed = std::chrono::high_resolution_clock::now() - begin;
 
